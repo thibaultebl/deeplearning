449db95fc81bd92305d393f2798c7c57c800d73d
thibaultebl <thibault.eubelen@gmail.com>
2025-10-25 12:59:30 +0200

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        double[][] dataset = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+
+    }
+}
---
+++ src/main/java/org/example/Trainer.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Trainer {
+    public double[] inputs;
+    public double[] outputs;
+
+    public Trainer(double[] inputs, double[] outputs) {
+        this.inputs = inputs;
+        this.outputs = outputs;
+    }
+
+
+}
